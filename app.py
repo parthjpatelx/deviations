30cb732f6a03c9ec87b87e1d0441758c998cd0a7
parthjpatelx <53063794+parthjpatelx@users.noreply.github.com>
2021-05-27 19:24:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import xlrd, datetime
 import xlwt
 from xlwt import Workbook
 
+def write_deviation(last_name, date_string, cycle_name, IRB_number):
+    my_deviation = f"Patient {last_name} was assessed on {date_string} for {cycle_name} on {IRB_number}. At this visit, the patient was dispensed drug. However, since the patient was unabel to travel to clinic due to circumstances surrounding COVID-19, the drug was shipped to them. This event constitues a protocol deviation."
+
+    return my_deviation
+
+
 
 # Give the location of the file- https://www.geeksforgeeks.org/reading-excel-file-using-python/
 loc = ('/Users/parth/drugshipments.xls')
@@ -17,9 +23,15 @@ sheet = wb.sheet_by_index(0)
 writing_wb = Workbook()
 sheet1 = writing_wb.add_sheet('Deviations')
 
+all_deviations= []
+
+IRB_number = "19-257"
+LAST_COLUMN = 6
 
 for i in range(sheet.nrows):
+    MRN = cycle_name = last_name = string_date = ""
     for j in range(sheet.ncols):
+        #if the column represents a date, we need to treat it differently
         if j == 1:
             #cast the date string into an integer
             xl_date = int(sheet.cell_value(i,1))
@@ -30,14 +42,36 @@ for i in range(sheet.nrows):
 
             #convert datetime object into string
             string_date = date_object.isoformat()
-            sheet1.write(i, j, f"dope : {string_date}")
+            sheet1.write(i, j, string_date)
         else:
-            sheet1.write(i,j, "hello")
+            cell_value = sheet.cell_value(i,j)
+            if j == 0:
+                MRN = cell_value
+            if j == 2:
+                cycle_name = cell_value
+            if j == 3:
+                last_name = cell_value
+
+            sheet1.write(i,j, cell_value)
+
+    deviation = write_deviation(last_name, string_date, cycle_name, IRB_number)
+    all_deviations.append(deviation)
+
+number_deviations = len(all_deviations)
+
+for n in range(number_deviations):
+    current_deviation = all_deviations[n]
+    sheet1.write(n,LAST_COLUMN, current_deviation)
 
 
-writing_wb.save('deviations.xls')
+
+
+writing_wb.save('deviations2.xls')
+
+
 
 
 #references:
 ##https://www.kite.com/python/answers/how-to-convert-an-excel-date-to-a-string-in-python
 #https://www.geeksforgeeks.org/reading-excel-file-using-python/
+
