f96f8b123dee005c07b4665dcbfcfd878d80bbff
parthjpatelx <53063794+parthjpatelx@users.noreply.github.com>
2021-05-27 18:45:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,19 +19,25 @@ sheet1 = writing_wb.add_sheet('Deviations')
 
 
 for i in range(sheet.nrows):
-    #https://www.kite.com/python/answers/how-to-convert-an-excel-date-to-a-string-in-python
+    for j in range(sheet.ncols):
+        if j == 1:
+            #cast the date string into an integer
+            xl_date = int(sheet.cell_value(i,1))
 
-    #cast the date string into an integer
-    xl_date = int(sheet.cell_value(i,1))
+            #convert integer into a datetime object
+            datetime_date = xlrd.xldate_as_datetime(xl_date, 0)
+            date_object = datetime_date.date()
 
-    #convert integer into a datetime object
-    datetime_date = xlrd.xldate_as_datetime(xl_date, 0)
-    date_object = datetime_date.date()
-
-    #convert datetime object into string
-    string_date = date_object.isoformat()
-    sheet1.write(i, 0, f"Hi : {string_date}")
-
+            #convert datetime object into string
+            string_date = date_object.isoformat()
+            sheet1.write(i, j, f"dope : {string_date}")
+        else:
+            sheet1.write(i,j, "hello")
 
 
 writing_wb.save('deviations.xls')
+
+
+#references:
+##https://www.kite.com/python/answers/how-to-convert-an-excel-date-to-a-string-in-python
+#https://www.geeksforgeeks.org/reading-excel-file-using-python/
