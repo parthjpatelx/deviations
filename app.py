04b1508a8429b0388bbe9bc21105c0c9570ce55e
parthjpatelx <53063794+parthjpatelx@users.noreply.github.com>
2021-05-27 18:29:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,17 @@ sheet1 = writing_wb.add_sheet('Deviations')
 
 for i in range(sheet.nrows):
     #https://www.kite.com/python/answers/how-to-convert-an-excel-date-to-a-string-in-python
+
+    #cast the date string into an integer
     xl_date = int(sheet.cell_value(i,1))
+
+    #convert integer into a datetime object
     datetime_date = xlrd.xldate_as_datetime(xl_date, 0)
     date_object = datetime_date.date()
 
+    #convert datetime object into string
     string_date = date_object.isoformat()
-    sheet1.write(i, 0, f"Date : {string_date}")
+    sheet1.write(i, 0, f"Hi : {string_date}")
 
 
 
